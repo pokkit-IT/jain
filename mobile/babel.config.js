13493d3a7ae57c470119992213adcdf4f8b6eb9a
Jim Shelly <jim.shelly@gmail.com>
2026-04-21 12:45:52 -0500

    fix(mobile): restore babel-preset-expo for Metro; use preset-env/react/ts for Jest

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-  ],
+  presets: [require('./node_modules/expo/node_modules/babel-preset-expo')],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        ['@babel/preset-react', { runtime: 'automatic' }],
+        '@babel/preset-typescript',
+      ],
+    },
+  },
 };

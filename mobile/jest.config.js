447504c1a47730ccbaa764a71e37260fef5996e7
Jim Shelly <jim.shelly@gmail.com>
2026-04-20 20:48:28 -0500

    chore: remove SightingPopup from core (moved to yardsailing plugin bundle)

---
+++ mobile/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.(test|spec).[jt]s?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.tsx',
+  ],
+};
